f54571ecc92b33af9326af6cf33f8a0423fb2630
sayplus1726 <sayplus1726@naver.com>
2023-08-07 17:09:48 +0900

    ResponseBody

---
+++ src/main/java/com/ll/exam/sbb/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.ll.exam.sbb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+  @RequestMapping("/sbb")
+  @ResponseBody
+  public String index() {
+    return "안녕하세요";
+  }
+}
